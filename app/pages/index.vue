561dcccc926678d315aad27f257db67d4c7c37a0
seros <github@seros.me>
2026-03-05 22:17:00 +0800

    1

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,10 +1,58 @@
+<script setup lang="ts">
+function openGoogle() {
+  window.open('https://google.com')
+}
+
+function openGithub() {
+  window.open('https://github.com')
+}
+
+const groups = [
+  {
+    id: 'pages',
+    label: 'Pages',
+    items: [
+      {
+        label: 'To Music',
+        icon: 'i-lucide-globe',
+        onSelect() {
+          navigateTo('/music')
+        },
+      },
+      {
+        label: 'To Test',
+        icon: 'i-lucide-globe',
+        onSelect() {
+          navigateTo('/test')
+        },
+      },
+    ],
+  },
+  {
+    id: 'apps',
+    label: 'Applications',
+    items: [
+      {
+        label: 'Open Google',
+        icon: 'i-lucide-globe',
+        onSelect() {
+          openGoogle()
+        },
+      },
+      {
+        label: 'Open Github',
+        icon: 'i-lucide-github',
+        onSelect() {
+          openGithub()
+        },
+      },
+    ],
+  },
+]
+</script>
+
 <template>
-  <div class="flex flex-col items-center gap-4 border border-blue-500">
-    <UButton to="/music">
-      音乐页面
-    </UButton>
-    <UButton to="/test">
-      测试页面
-    </UButton>
-  </div>
+  <UCommandPalette
+    :groups="groups"
+  />
 </template>
